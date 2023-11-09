973d9acf62e48f39a5500016b01b7b8ef7a6a27f
James Azam <james.m.azam@gmail.com>
2023-11-09 18:44:24 +0000

    Add function to sample normally distributed samples

---
+++ R/get_norm_samples.R
@@ -0,0 +1,20 @@
+#' Get random normal samples for each element of list
+#'
+#' @param mylist list of numeric vectors
+#' @return A list of random normal samples with same length as the list elements
+#' @export
+#'
+#' @examples
+#' mylist <- list(a = 1, b = 1:10, c = 1:20)
+#' get_norm_samples(mylist)
+get_norm_samples <- function(mylist){
+  ll_rand <- list()
+  
+  for (i in 1:length(mylist)) {
+    element_length <- length(mylist[[i]])
+    rnorm_samples <- rnorm(ll)
+    ll_rand[[i]] <- rnorm_samples
+    }
+  
+  return(ll_rand)
+}
